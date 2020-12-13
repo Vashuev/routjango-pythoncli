efdfe6af3fc9807a888a44069c54cf46eb063378
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-13 13:26:34 +0530

    open frontend in user's webbrowser

--- init.py
+++ init.py
@@ -2,14 +2,18 @@ import asyncio
 import socketio
 import os
 import requests
+import webbrowser
 
 sio = socketio.AsyncClient(reconnection=False)
+server_url = 'https://codr-front-server.herokuapp.com/'
+webpage_url = 'https://code-front.vercel.app/'
 
 
 @sio.event
 async def connect():
     try:
         print('connected to server')
+        webbrowser.open_new_tab(f'{webpage_url}?roomId={roomid}')
         os.system(f"python routjango\main.py")
     except:
         print("  ERROR : Unable to call main.py file")
@@ -32,12 +36,12 @@ def message(msg):
 
 
 async def start_server(roomid):
-    await sio.connect(f'https://codr-front-server.herokuapp.com/?roomId={roomid}&clientType=cli')
+    await sio.connect(f'{server_url}?roomId={roomid}&clientType=cli')
     await sio.wait()
 
 
 if __name__ == '__main__':
-    response = requests.post('https://codr-front-server.herokuapp.com/login')
+    response = requests.post(f'{server_url}login')
     if response.status_code == 200:
         content = response.json()
         # print(content)
