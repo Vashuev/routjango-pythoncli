8bdf6a0bc34bb84bd5ac7b6d18d0900fc4c8e9e9
rohit bisht <rohitbst.104764@gmail.com>
2020-12-13 13:08:18 +0530

    main.py to routjango

--- init.py
+++ init.py
@@ -2,7 +2,6 @@ import asyncio
 import socketio
 import os
 import requests
-import json
 
 sio = socketio.AsyncClient(reconnection=False)
 
@@ -11,7 +10,7 @@ sio = socketio.AsyncClient(reconnection=False)
 async def connect():
     try:
         print('connected to server')
-        # os.system(f"python main.py")
+        os.system(f"python routjango\main.py")
     except:
         print("  ERROR : Unable to call main.py file")
 
@@ -33,7 +32,7 @@ def message(msg):
 
 
 async def start_server(roomid):
-    await sio.connect(f'https://codr-front-server.herokuapp.com/?roomId=af7e3463-0168-4860-a016-648718b97dc0&clientType=cli')
+    await sio.connect(f'https://codr-front-server.herokuapp.com/?roomId={roomid}&clientType=cli')
     await sio.wait()
 
 
